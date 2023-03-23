fb8fe0228b758ad2ac087d0460fc2e1535db335e
exp-ext <ext77@yandex.ru>
2023-03-23 15:38:30 +0300

    create

--- backend/recipes/tests/test_tags.py
+++ backend/recipes/tests/test_tags.py
@@ -49,8 +49,7 @@ class TagsTestCase(APITestCase):
         url = reverse('recipes:tags-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.get('count'), 2)
-        result = response.data.get('results')
+        result = response.data
         self.assertEqual(result[0].get('name'), self.tag1.name)
         self.assertEqual(result[1].get('name'), self.tag2.name)
 
